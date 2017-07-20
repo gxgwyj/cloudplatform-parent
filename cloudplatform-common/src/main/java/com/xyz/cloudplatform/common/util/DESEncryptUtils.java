fdf6f19333daed585e649647f5d9755d2988120a
gxgwyj <498868779@qq.com>
2017-07-20 17:21:10 +0800

    添加工具类

---
+++ cloudplatform-common/src/main/java/com/xyz/cloudplatform/common/util/DESEncryptUtils.java
@@ -0,0 +1,9 @@
+package com.xyz.cloudplatform.common.util;
+
+/**
+ * Created by Administrator on 2017/7/20.
+ */
+public class DESEncryptUtils {
+    private static final String ALGORITHM = "DES";
+
+}
